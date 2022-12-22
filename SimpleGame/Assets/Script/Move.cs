38c045ac087f91fbd9bcf4217ee2139601dbb791
fsfujinity <huishitengzhi50@gmail.com>
2022-12-22 15:44:49 +0900

    12/22
    オブジェクトを配置
    スクリプト作成

---
+++ SimpleGame/Assets/Script/Move.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class Move : MonoBehaviour
+{
+    public float moveSpeed;
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+}
